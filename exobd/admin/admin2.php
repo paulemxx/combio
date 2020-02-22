e9738b6afdf5811ca3c8c30d5005b4b3cb81e16a
MJoshua25 <jyao2000@yahoo.fr>
2020-02-22 12:29:13 +0000

    fin

--- exobd/admin/admin2.php
+++ exobd/admin/admin2.php
@@ -5,17 +5,16 @@ session_start();
 $bdd = new PDO("mysql:host=localhost;dbname=combio",'root','');
 $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); // Gestion des erreurs.
 
-$requser = $bdd->query("SELECT * FROM clients");
+$sql = ['SELECT * FROM clients;', 'select * from commandes;', 'select * from categories;', 'select * from produits;', 'select * from contenir;'];
 
-$result = $requser->fetch();
+foreach ($sql as $value){
+    $requser = $bdd->query($value);
 
+    $result = $requser->fetchAll();
 
-echo $result['nom'];  
-echo '<br>';
-echo $result['prenoms'];  
-echo $result['ville']; 
-echo $result['contact']; 
-echo $result['cli_parrain']; 
+    print_r($result);
+}
+
 
    
       
